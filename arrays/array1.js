6780cd316b40868d957149bf88356c86e88c14c7
katongole89 <katongolerichard089@gmail.com>
2022-07-19 10:31:20 +0300

    added filter function

--- arrays/array1.js
+++ arrays/array1.js
@@ -1,8 +1,7 @@
-
+let nameArr = ['cat', 'dog', 'cow']
 // +++++++++++++++++++++++++++++++++ start +++++++++++++++++++++++++++++++++++++
 // FOREACH
 // helps you loop through every thing without returning anything
-let nameArr = ['cat', 'dog', 'cow']
 const forEachFunc = (arr) =>{
 	arr.forEach((name)=>{
 		console.log(name)
@@ -10,5 +9,19 @@ const forEachFunc = (arr) =>{
 }
 //call function to see result
 // forEachFunc(nameArr)
+// +++++++++++++++++++++++++++++++++ end +++++++++++++++++++++++++++++++++++++
+
+
+// +++++++++++++++++++++++++++++++++ start +++++++++++++++++++++++++++++++++++++
+//FILTER
+//returns an array of elements that meet a certain 
+const filterFunc = (arr) =>{
+	const newArray = arr.filter((name)=>{
+		return name != 'cow'
+	})
+	console.log(newArray)
+}
+//call function to see result
+// filterFunc(nameArr)
 // +++++++++++++++++++++++++++++++++ end +++++++++++++++++++++++++++++++++++++
 
