9af81ea9d787c0ff34bd0fe1ee87302af412c761
katongole89 <katongolerichard089@gmail.com>
2022-07-19 10:18:44 +0300

    added foreach function

---
+++ arrays/array1.js
@@ -0,0 +1,14 @@
+
+// +++++++++++++++++++++++++++++++++ start +++++++++++++++++++++++++++++++++++++
+// FOREACH
+// helps you loop through every thing without returning anything
+let nameArr = ['cat', 'dog', 'cow']
+const forEachFunc = (arr) =>{
+	arr.forEach((name)=>{
+		console.log(name)
+	})
+}
+//call function to see result
+// forEachFunc(nameArr)
+// +++++++++++++++++++++++++++++++++ end +++++++++++++++++++++++++++++++++++++
+
