b7195a24b254238ad19a9940315a2abeaff855e4
katongole89 <katongolerichard089@gmail.com>
2022-09-22 16:57:38 +0300

    added charAt function

---
+++ Strings/strings.js
@@ -0,0 +1,11 @@
+// +++++++++++++++++++++++++++++++++ start +++++++++++++++++++++++++++++++++++++
+// charAt(x)
+// This function will return the character at the x position within the string.
+let testString = 'forever young'
+const charAtFunc = (str) =>{
+	let theCharacter = str.charAt(6)
+    console.log(theCharacter)
+}
+//call function to see result
+charAtFunc(testString)
+// +++++++++++++++++++++++++++++++++ end +++++++++++++++++++++++++++++++++++++
