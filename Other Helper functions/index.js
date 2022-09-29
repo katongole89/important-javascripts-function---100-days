abb6bf7d3010e73f8b5379b21b371195de9e451a
katongole89 <katongolerichard089@gmail.com>
2022-09-29 13:01:35 +0300

    added the setInterval func

---
+++ Other Helper functions/index.js
@@ -0,0 +1,11 @@
+
+
+// +++++++++++++++++++++++++++++++++ start +++++++++++++++++++++++++++++++++++++
+// setInterval(func, millseconds)
+// Display "Hello" every second (1000 milliseconds):
+const setIntervalFunc = () => {
+	setInterval(()=>console.log('check interval'), 2000)
+}
+//call function to see result
+// setIntervalFunc()
+// +++++++++++++++++++++++++++++++++ end +++++++++++++++++++++++++++++++++++++
